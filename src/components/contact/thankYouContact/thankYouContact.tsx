21d84cc710acbab5b3c3421e04233d253c07ad6e
yossefbanhaim <73604318+yossefbenhaim@users.noreply.github.com>
2024-07-19 14:45:51 +0300

    fix(): lottie animation

--- src/components/contact/thankYouContact/thankYouContact.tsx
+++ src/components/contact/thankYouContact/thankYouContact.tsx
@@ -1,6 +1,7 @@
 import Heder from '../header/heder'
-import ThankYouAnimation from '../thankYouAnimation/thankYouAnimation'
 
+import test from '../../../assets/lottie/thankYouAnimation.json'
+import Lottie from 'lottie-react'
 const THANK = 'Thank'
 const YOU = 'You'
 const TO_CONTACT_WITH_ME = 'To Contact With Me'
@@ -9,7 +10,9 @@ const ThankYouContact = () => {
 	return (
 		<div className='w-full h-[100vh] flex flex-col justify-start pt-[4%] items-center'>
 			<Heder title={TO_CONTACT_WITH_ME} openingSentence1={THANK} openingSentence2={YOU} />
-			<ThankYouAnimation />
+			<div className='w-[400px] h-[400px] flex flex-col justify-center items-center '>
+				<Lottie loop animationData={test} />
+			</div>
 		</div>
 	)
 }
