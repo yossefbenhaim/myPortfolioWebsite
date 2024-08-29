99d123c344565af88ed04b5af3410dc9cb141a3a
yossefbanhaim <73604318+yossefbenhaim@users.noreply.github.com>
2024-08-29 11:07:39 +0300

    fix(): navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -33,60 +33,52 @@ const Navbar = ({ scrollToSection, activeSection }: Props) => {
 	};
 
 	return (
-		<div className='z-50 sticky top-0 pl-5 pr-[10%] w-full h-[70px] bg-[#15152a]/20 items-end flex flex-row justify-between backdrop-blur-lg'>
-			<div className='flex flex-row gap-2 h-full justify-center items-center w-[30%]'>
+		<div className='z-50 sticky top-0  pr-[10%] w-full h-[70px] bg-[#15152a]/20 items-end flex flex-row justify-between backdrop-blur-lg'>
+			<div className='flex flex-row gap-2 h-full justify-end items-center w-[250px]'>
 				<p className='text-white/90 font-semibold'>{DEVELOPER_FIRST_NAME}</p>
 				<p className='text-primary-color font-semibold'>{DEVELOPER_LAST_NAME}</p>
 			</div>
-			<div className='w-[80%] flex justify-end items-center'>
-				{/* Hamburger Menu Icon */}
-				<div className='lg:hidden'>
-					<button onClick={toggleMobileMenu} className='text-white/80'>
-						{isMobileMenuOpen ? (
+			<div className='w-[80%] flex justify-end items-center  h-full'>
+				<div className='lg:hidden  h-full flex flex-col justify-center items-center'>
+					<button onClick={toggleMobileMenu} className='text-white/80 z-50'>
+						{!isMobileMenuOpen && (
 							<svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M6 18L18 6M6 6l12 12'
-								/>
-							</svg>
-						) : (
-							<svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M4 6h16M4 12h16m-7 6h7'
-								/>
+								<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7' />
 							</svg>
 						)}
 					</button>
 				</div>
-
-				{/* Mobile Menu */}
 				<div
 					className={`lg:hidden fixed inset-0 bg-black bg-opacity-50 z-40 transition-transform transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
 						}`}
 				>
-					<ul className='flex flex-col items-center justify-center h-[250px] gap-8 bg-black/40 '>
-						{NavbarPathOptions.map((option) => (
-							<li key={option.item}>
-								<a
-									onClick={() => navigationPage(option.path)}
-									className={`${activeSection === option.path ? 'text-primary-color' : 'text-white/80'
-										} text-xl tracking-widest font-bold`}
-								>
-									{option.item}
-								</a>
-							</li>
-						))}
-					</ul>
+					<div className='relative w-full h-full'>
+						<button
+							onClick={toggleMobileMenu}
+							className='absolute top-4 right-4 text-white/80 mr-5  p-2 rounded-full z-50'
+						>
+							<svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+								<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
+							</svg>
+						</button>
+						<ul className='flex flex-col items-center justify-center h-[250px] gap-8 absolute w-full bg-black/40'>
+							{NavbarPathOptions.map((option) => (
+								<li key={option.item}>
+									<a
+										onClick={() => navigationPage(option.path)}
+										className={`${activeSection === option.path ? 'text-primary-color' : 'text-white/80'
+											} text-xl tracking-widest font-bold`}
+									>
+										{option.item}
+									</a>
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 
-				{/* Desktop Menu */}
-				<div className='hidden lg:block'>
-					<ul className='flex flex-row gap-[6%] items-center'>
+				<div className=' hidden lg:block  w-[50%]'>
+					<ul className=' flex flex-row justify-center gap-[10%] items-center'>
 						{NavbarPathOptions.map((option) => (
 							<li key={option.item}>
 								<a
